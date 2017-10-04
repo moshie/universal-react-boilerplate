0f4fadf2111294f170e50f778281f171b974ca31
David Hewitt <hello@wp-unite.io>
2017-10-04 22:18:39 +0100

    boilerplate fields validation

--- src/api/authentication/validation/registration-validation.js
+++ src/api/authentication/validation/registration-validation.js
@@ -2,5 +2,19 @@ const { check } = require('express-validator/check')
 const { matchedData, sanitize } = require('express-validator/filter')
 
 export default [
-    check('email').exists().isEmail()
+    check('email')
+        .exists()
+        .isEmail(),
+
+    check('first_name')
+        .exists(),
+
+    check('last_name'),
+
+    check('password')
+        .exists(),
+
+    check('password_confirmation')
+        .exists()
+        .custom((value, { req }) => value === req.body.password)
 ]
