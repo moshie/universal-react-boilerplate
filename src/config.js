308d797247c0857799a8497b1eee5a0897da4951
David Hewitt <hello@wp-unite.io>
2017-10-04 22:07:22 +0100

    Added body parser & Mongo DB connection

--- src/config.js
+++ src/config.js
@@ -8,3 +8,5 @@ exports.port = PORT
 exports.key = fs.readFileSync(path.resolve(__dirname, '..', './server.key'))
 
 exports.cert = fs.readFileSync(path.resolve(__dirname, '..', './server.crt'))
+
+exports.db = process.env.MONGODB || 'mongodb://127.0.0.1:27017/test'
--- src/server.js
+++ src/server.js
@@ -1,20 +1,35 @@
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 import express from 'express'
 import morgan from 'morgan'
 import http2 from 'spdy'
-import { port, key, cert } from './config'
+import { port, key, cert, db } from './config'
 import ApiRoutes from './api/routes'
 import HttpRoutes from './http/routes'
 
+// Boot app
 const app = express()
 
+// Logger
 app.use(morgan('dev'))
 
+// Database
+mongoose.connect(db, { useMongoClient: true })
+
+// Http request parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Static assets
 app.use(express.static('static'))
 
+// Api endpoints
 app.use('/api/v1', ApiRoutes)
 
+// Http routes
 app.use(HttpRoutes)
 
+// Start server
 http2.createServer({key, cert}, app)
     .listen(port, () => {
         console.log(`Server started on https://localhost:${port}`)
