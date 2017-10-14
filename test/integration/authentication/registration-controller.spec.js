26a543535c3af170f4b1ea7c9be8f6b14772f60a
David Hewitt <hello@wp-unite.io>
2017-10-15 00:58:14 +0100

    Started tests on registration controller

--- test/integration/authentication/registration-controller.spec.js
+++ test/integration/authentication/registration-controller.spec.js
@@ -1,15 +1,87 @@
 // Vendor
 import { expect } from 'chai'
+import sinon from 'sinon'
+
+import registrationController from '../../../src/api/authentication/controllers/registration-controller'
+
+import User from '../../../src/api/authentication/models/User'
+
+const userFixture = {
+    "email": "batman@example.com",
+    "first_name": "Bruce",
+    "last_name": "Wayne",
+    "password": "b4tm4nIsTh3B3st",
+    "password_confirmation": "b4tm4nIsTh3B3st"
+}
+
+const _validationErrors = [{ 
+    location: 'body',
+    param: 'email',
+    value: '',
+    msg: 'Please provide a valid Email' 
+}]
+
 
 describe('Registration Controller', () => {
 
-    it('should return a 422 response')
+    var user
 
-    it('should return an object with validation results')
+    beforeEach(() => {
+        user = sinon.stub(User.prototype, 'save').resolves({
+            fullname: function () {}
+        })
+    })
 
-    it('should create a new user without passing password_confirmation')
+    afterEach(() => {
+        user.restore()
+    })
 
-    it('should return a 200 response')
+    it('should return a 422 response', () => {
+        let req = { _validationErrors }
+        let res = {
+            status: function (responseCode) {
+                expect(responseCode).to.be.a('number')
+                expect(responseCode).to.equal(422)
+                return this
+            },
+            json: function () {}
+        }
+
+        registrationController(req, res)
+    })
+
+    it('should return an object with validation results', () => {
+        var req = { _validationErrors }
+        var res = {
+            status: function () { return this },
+            json: function (json) {
+                expect(json).to.be.an('object')
+                expect(json).to.have.own.property('errors')
+                expect(json.errors).to.have.property('email')
+            }
+        }
+
+        registrationController(req, res)
+    })
+
+    it('should return a 200 response', () => {
+        
+        var req = { body: userFixture }
+        var res = {
+            json: function () { return this },
+            status: sinon.stub().returnsThis()
+        }
+
+        registrationController(req, res)
+
+        return user()
+            .then(() => {
+                expect(res.status.called).to.be.true
+                expect(res.status.firstCall.args[0]).to.be.a('number')
+                expect(res.status.firstCall.args[0]).to.equal(200)
+            })
+
+    })
 
     it('should save the user')
 
